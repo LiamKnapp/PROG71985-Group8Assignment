474a47075b62a035d545819c6817b4dfb4dabe61
Liam Knapp <Lknapp1110@conestogac.on.ca>
2021-11-17 13:42:55 -0500

    Added multiple files did the propertites thing. started the code. havnt tested and lots of errors.

---
+++ PROG71985-Group8Assignment/AddAppt.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "Structure.c"
+
+#define _CRT_SECURE_NO_WARNINGS
+#define LINENUM 1
+#define ABORT 0
+#define MAXDAYSINMONTH 30
+
+char addappointment(Calendar* date) { // books an appointment
+	int  count, input = 0;
+	char appt[MAXSIZE];
+	for (count = 0; count < MAXDAYSINMONTH; count++) { //  displays all current days that have no appointment
+		if (strcmp(date[count].available, "Available") == 0) {
+			printf("\nDays with no appointments are:\n%s", date[count].day);
+		}
+	}
+
+	printf("\nWhat day would you like to add a appointment to? type 0 to abort: \n");
+	scanf("%d", &input);
+
+	if (input == ABORT) {// abort if user pressed 0
+		display();// print the display
+		return 0;
+	}
+
+	printf("\nEnter your first appointment: \n"); // gather the appointment info
+	scanf("%s", &appt);
+
+	strcpy(date[input - LINENUM].appointment, &appt); // add appointment
+	strcpy(date[input - LINENUM].available, "Unavailable"); // make date tag as Unavailable
+	printf("\nbook complete\n");
+
+	display();
+	return 0;
+}
--- PROG71985-Group8Assignment/Main.c
+++ PROG71985-Group8Assignment/Main.c
@@ -3,36 +3,46 @@
 #include <stdbool.h>
 
 #include "Structure.c"
+#include "AddAppt.c"
+#include "DeleteAppt.c"
+#include "DisplayAppts.c"
+#include "SearchAppt.c"
+#include "UpdateAppt.c"
 
 #define _CRT_SECURE_NO_WARNINGS
 #define LINENUM 1
 #define ABORT 0
+#define MAXDAYSINMONTH 30
 
 bool continueprogram = true;// loop for the program
 FILE* fp; // declare the file
 
 char display() { // print the display / main menu without the option f
 	printf("\nTo choose a function, enter its letter label: \n");
-	printf("a)  \n");
-	printf("b)  \n");
-	printf("c)  \n");
-	printf("d)  \n");
-	printf("e) \n");
+	printf("a) Add a new appt \n");
+	printf("b) Delete an existing appt \n");
+	printf("c) Update and existing appt \n");
+	printf("d) Display single appt \n");
+	printf("e) Display range appt \n");
+	printf("f) Display all appt \n");
+	printf("g) Search for appt \n");
+	printf("h) Exit \n");
 
 	return 0;
 }
+
 int main(void) {
 	char Select;
 	int count = 0;
 
-	Room rooms[MAXSIZE];
+	Calendar date[MAXSIZE];
 
-	FILE* fp = fopen("Roominfo.txt", "r+"); // read the file with saved data
+	FILE* fp = fopen("calendar.txt", "r+"); // read the file with saved data
 	if (fp == NULL) { // if error close
 		printf("ERROR");
 		exit(ABORT);
 	}
-	while (fscanf(fp, "%s %s %s %s ", rooms[count].roomid, rooms[count].available, rooms[count].firstname, rooms[count].lastname) != EOF) {
+	while (fscanf(fp, "%s %s %s %s %s", date[count].day, date[count].month, date[count].year,date[count].available, date[count].appointment) != EOF) {
 		count++; // save the file information into my struct
 	}
 	fclose(fp);
@@ -42,39 +52,48 @@ int main(void) {
 
 		scanf("%c", &Select); // get the user selection
 
-		if (Select == 'a') { // 
+		if (Select == 'a') { // Add a new appt
+			addappointment(date);
+		}
+
+		if (Select == 'b') { // Delete an existing appt
+			deleteappointment(date);
+		}
+
+		if (Select == 'c') { //  Update and existing appt
 			
 		}
 
-		if (Select == 'b') { // 
-			
+		if (Select == 'd') { // Display single appt
+			SingleAppt(date);
 		}
 
-		if (Select == 'c') { // 
-			
+		if (Select == 'e') { //Display range appt
+			RangeAppt(date);
 		}
 
-		if (Select == 'd') { //
-			
+		if (Select == 'f') { // Display all appt
+			AllAppt(date);
 		}
 
-		if (Select == 'e') { //
-			
+		if (Select == 'g') { // Search for appt
+		
 		}
 
-		if (Select == 'f') { // exit the program
+		if (Select == 'h') { // Exit the program
 			printf("\n");
 			continueprogram = false;
 		}
 
-		FILE* fp = fopen("Roominfo.txt", "r+"); // open the file again for saving
+		FILE* fp = fopen("calendar.txt", "r+"); // open the file again for saving
 		if (fp == NULL) { // if error close
 			printf("ERROR");
 			exit(ABORT);
 		}
-		for (count = 0; count < MAXROOMS; count++) { // print new information into the file for saving
-			fprintf(fp, "%d %s %s %s\n", count + LINENUM, &rooms[count].available, &rooms[count].firstname, &rooms[count].lastname);
+		for (count = 0; count < MAXDAYSINMONTH; count++) { // print new information into the file for saving
+			fprintf(fp, "%d %s %s %s\n", count + LINENUM, &date[count].month, &date[count].year, &date[count].appointment);
 		}
 		fclose(fp); // save file by closeing
 	}
+	
 }
