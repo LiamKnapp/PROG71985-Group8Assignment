ce0f27122501b05f497a428a03b8ad2ef55db058
Liam Knapp <Lknapp1110@conestogac.on.ca>
2021-12-04 18:31:07 -0500

    finished the update appointment function

--- PROG71985-Group8Assignment/UpdateAppt.c
+++ PROG71985-Group8Assignment/UpdateAppt.c
@@ -10,6 +10,32 @@
 
 
 char updateappointment(P_CALENDAR date) {
+	int count, input = 0;
+	char appt[MAXSIZE];
+
+	for (count = 0; count < MAXDAYSINMONTH; count++) { //  displays all current days that have no appointment
+		if (strcmp(date[count].available, "Unavailable") == 0) {
+			printf("\nDays with appointments are:\n%d, %s", date[count].day, date[count].appointment);
+		}
+	}
+
+
+	printf("\nWhat day would you like to update a appointment for? type 0 to abort: \n");
+	scanf_s("%d", &input);
+	fseek(stdin, 0, SEEK_END);
+	fseek(stdin, 0, SEEK_END);
+
+	if (input == ABORT) {// abort if user pressed 0
+		fseek(stdin, 0, SEEK_END);
+		return 0;
+	}
+
+	printf("\nEnter your appointment: \n"); // gather the appointment info
+	scanf("%s", appt);
+	fseek(stdin, 0, SEEK_END);
+
+	strcpy(date[input - LINENUM].appointment, &appt); // remove the appointment
+	printf("\nAppointment updated\n");
 
 	return 0;
 }
